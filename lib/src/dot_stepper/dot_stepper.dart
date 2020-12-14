cc5a467f8fbfd88272731b09c086f587b9b8e1af
imujtaba8488_mac <developer.imujtaba@gmail.com>
2020-12-14 22:51:40 +0530

    Created DotStepper

--- lib/src/dot_stepper/dot_stepper.dart
+++ lib/src/dot_stepper/dot_stepper.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
 class DotStepper extends StatefulWidget {
+  DotStepper({this.dotCount, this.radius});
+  
+  final int dotCount;
+  final double radius;
+
   @override
   _DotStepperState createState() => _DotStepperState();
 }
 
 class _DotStepperState extends State<DotStepper>
     with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      duration: Duration(milliseconds: 400),
+      vsync: this,
+    )..addListener(() {
+        setState(() {});
+      });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 }
