4568e05ae9faf7e208db2974a4049bda5422171b
imujtaba8488_mac <developer.imujtaba@gmail.com>
2020-12-15 23:46:57 +0530

    center and dotRadius not privite to DotOffset

--- lib/src/dot_stepper/dot_offset.dart
+++ lib/src/dot_stepper/dot_offset.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 class DotOffset {
-  DotOffset(this._center, this.dotRadius);
+  DotOffset(this._center, this._dotRadius);
 
   Offset _center;
-  double dotRadius;
+  double _dotRadius;
 
-  double get left => _center.dx - dotRadius;
+  double get left => _center.dx - _dotRadius;
 
-  double get top => _center.dy - dotRadius;
+  double get top => _center.dy - _dotRadius;
 
-  double get right => _center.dx + dotRadius;
+  double get right => _center.dx + _dotRadius;
 
-  double get bottom => _center.dy + dotRadius;
+  double get bottom => _center.dy + _dotRadius;
 
-  double get center => dotRadius;
+  double get center => _dotRadius;
 }
