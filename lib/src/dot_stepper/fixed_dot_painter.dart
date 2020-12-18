db0071268b2b8676185f94e324f980d6eca21523
imujtaba8488_mac <developer.imujtaba@gmail.com>
2020-12-18 20:29:47 +0530

    FixedDotPainter: added lineConnectorDisabled property.

--- lib/src/dot_stepper/fixed_dot_painter.dart
+++ lib/src/dot_stepper/fixed_dot_painter.dart
@@ -12,24 +12,44 @@ class FixedDotPainter extends CustomPainter {
     this.dotCount = 1,
     this.direction = Axis.horizontal,
     this.shape = Shape.circle,
+    this.lineConnectorsEnabled = true,
     @required this.dotOffsets,
     @required this.brush,
     @required this.lineConnectorBrush,
     @required this.tappedAt,
   });
 
+  /// The total number of dots to paint.
   final int dotCount;
+
+  /// The radius of a dot.
   final double dotRadius;
+
+  /// The list of offsets where the dots are to be painted.
   final List<DotOffset> dotOffsets;
+
+  /// The direction in which to display the dots on the screen, i.e., horizontally or vertically.
   final Axis direction;
+
+  /// The shape of the fixed dots.
   final Shape shape;
+
+  /// The `Paint` object used for drawing the fixed dots.
   final Paint brush;
+
+  /// The `Paint` object used for drawing the line connectors.
   final Paint lineConnectorBrush;
+
+  /// Callback which provides the index of the dot that was tapped.
   final TappedAt tappedAt;
+
+  /// Whether to enable or disable line connectors between dots.
+  final bool lineConnectorsEnabled;
 
   @override
   void paint(Canvas canvas, Size size) {
     for (int index = 0; index < dotCount; index++) {
+      // Create an instance of the `ShapePainter`, supply the required properties and use its draw method to draw the shape based on the `shape` property provide.
       ShapePainter shapePainter = ShapePainter(
         canvas,
         brush,
@@ -44,10 +64,13 @@ class FixedDotPainter extends CustomPainter {
       shapePainter.draw(shape);
     }
 
-    drawLineConnectors(canvas);
+    // Mind the if statement!
+    if (lineConnectorsEnabled) _drawLineConnectors(canvas);
   }
 
-  void drawLineConnectors(Canvas canvas) {
+  /// Draw the line connectors.
+  void _drawLineConnectors(Canvas canvas) {
+    // (index < dotCount -1) because omit the line connector after the last dot.
     for (int index = 0; index < dotCount - 1; index++) {
       canvas.drawLine(
         direction == Axis.horizontal
